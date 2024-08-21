fb3266e58e2d450f8e902492bb0a49c3a3d61d45
Happy1speed <144567994+Happy1speed@users.noreply.github.com>
2024-08-21 14:49:34 -0700

    Made Guarding Tags only apply when guarding is present

--- src/main/java/net/happyspeed/balancedshield/BalancedShieldMod.java
+++ src/main/java/net/happyspeed/balancedshield/BalancedShieldMod.java
@@ -1,15 +1,29 @@
 package net.happyspeed.balancedshield;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Optional;
 
 public class BalancedShieldMod implements ModInitializer {
 	public static final String MOD_ID = "balancedshield";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static boolean GuardingModLoaded = false;
 
 	@Override
 	public void onInitialize() {
+		Optional<ModContainer> modContainer = FabricLoader.getInstance().getModContainer(BalancedShieldMod.MOD_ID);
+		if (FabricLoader.getInstance().isModLoaded("guarding")) {
+			GuardingModLoaded = true;
+			ResourceManagerHelper.registerBuiltinResourcePack(new Identifier(BalancedShieldMod.MOD_ID, "balanced_shield_guarding_compat"), (ModContainer) modContainer.get(), Text.translatable("pack.balancedshieldguardingcompat"), ResourcePackActivationType.ALWAYS_ENABLED);
+		}
 		LOGGER.info("Balanced Shield Initialized");
 	}
 }
